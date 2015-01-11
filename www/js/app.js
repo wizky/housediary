3f6a6a3212c55f2f4cd6d389a4d0d649e275f992
macbook pro <wayliu@live.com>
2015-01-11 12:34:30 -0800

    plugin working

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('housediary', ['ionic','housediary.controllers'])
+angular.module('housediary', ['ionic','housediary.controllers', 'starter.services'])
 
     .run(function ($rootScope, $ionicPlatform, $state, $window, OpenFB) {
         OpenFB.init('581957658577189');
@@ -67,6 +67,16 @@ angular.module('housediary', ['ionic','housediary.controllers'])
                     'menuContent': {
                         templateUrl: "views/logout.html",
 //                        controller: "LogoutCtrl"
+                    }
+                }
+            })
+
+            .state('app.wechat', {
+                url: "/wechat",
+                views: {
+                    'menuContent': {
+                        templateUrl: "views/wechat.html",
+                        controller: "DemoCtrl"
                     }
                 }
             });
